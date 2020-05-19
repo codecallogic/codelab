386072551f45a808fc5ab742196dc454503a05c9
Fabricio <jfguardiacpw@gmail.com>
2020-05-18 22:29:04 -0700

    implemented BEM architecture to css styles

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,15 +6,15 @@ class Header extends Component {
     render () {
         return (
             <div className="header">
-                <div className="logo-box">
-                    <img src="https://i.imgur.com/TIDi3Mb.png" alt="Logo" className="logo"/>
+                <div className="header__logo-box">
+                    <img src="https://i.imgur.com/TIDi3Mb.png" alt="Logo" className="header__logo"/>
                 </div>
-                <div className="text-box">
+                <div className="header__text-box">
                     <h1 className="heading-primary">
-                        <span className="heading-primary-main">Fluid Websites</span>
-                        <span className="heading-primary-sub">Fits This Page In Your Phone</span>
+                        <span className="heading-primary--main">Fluid Websites</span>
+                        <span className="heading-primary--sub">Fits This Page In Your Phone</span>
                     </h1>
-                    <Link to="/" className="btn btn-white btn-animated">Lets get start on a project together</Link>
+                    <Link to="/" className="btn btn--white btn--animated">Lets get start on a project together</Link>
                 </div>
             </div>
         )
