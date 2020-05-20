921ab56ba676e1d4a3e5436219016880b91aaddc
Fabricio <jfguardiacpw@gmail.com>
2020-05-19 23:57:51 -0700

    added homepage component and homepage page

--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import './App.css';
 import Header from '../../components/header/header'
 import CustomGrid from '../../components/grid/custom_grid'
+import Homepage from '../homepage/homepage';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Homepage />
         {/* <CustomGrid /> */}
       </div>
     )
---
+++ src/Pages/homepage/homepage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Homepage from '../../components/main/homepage'
+
+class Homepage extends Component {
+    render () {
+        return (
+            <div>
+                <Homepage />
+            </div>
+        )
+    }
+}
+export default Homepage
