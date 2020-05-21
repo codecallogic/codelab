5821bfdc0ef930e8516ecf4989f66b2320bb9769
Fabricio <jfguardiacpw@gmail.com>
2020-05-20 17:36:08 -0700

    added about section content and styled it

--- src/components/homepage/_sectionAbout.jsx
+++ src/components/homepage/_sectionAbout.jsx
@@ -4,7 +4,7 @@ class Homepage extends Component {
     render () {
         return (
             <section className="section-about">
-                <div className="u-center-text u-margin-bottom-8">
+                <div className="u-center-text u-margin-bottom-big">
                 <h2 className="heading-secondary">
                     My Favorite Projects
                 </h2>
@@ -12,10 +12,23 @@ class Homepage extends Component {
 
                 <div className="row">
                     <div className="col-1-of-2">
-                        <h3 className="heading-tertiary">
+                        <h3 className="heading-tertiary u-margin-small">
                             You're going to fall in love with mloop
                         </h3>
-                        <p className="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi et ex assumenda, iusto rem magnam dolore alias sed tempore?</p>
+                        <p className="paragraph">mLoops allows access to Spotify's millions of songs and curated playlists. Users can create posts related to their music search using tags to categorize their posts. They can also provide comments on other posts created by other users. </p>
+
+                        <h3 className="heading-tertiary u-margin-small">
+                            Get yourself a prechecked online with Recovery
+                        </h3>
+                        <p className="paragraph">mLoops allows access to Spotify's millions of songs and curated playlists. Users can create posts related to their music search using tags to categorize their posts. They can also provide comments on other posts created by other users. </p>
+
+                        <h3 className="heading-tertiary u-margin-small">
+                            Make music together in real-time with Instatone
+                        </h3>
+                        <p className="paragraph">mLoops allows access to Spotify's millions of songs and curated playlists. Users can create posts related to their music search using tags to categorize their posts. They can also provide comments on other posts created by other users. </p>
+
+                        <a href="" className="btn-text">Learn More &rarr; </a>
+
                     </div>
                     <div className="col-1-of-2">Images</div>
                 </div>
