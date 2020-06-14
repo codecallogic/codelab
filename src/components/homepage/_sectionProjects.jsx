13e9ae85464d94751ec80355868f2ea6e3eed32d
Fabricio <jfguardiacpw@gmail.com>
2020-06-14 14:37:24 -0700

    completed 3 projects cards for projects section

--- src/components/homepage/_sectionProjects.jsx
+++ src/components/homepage/_sectionProjects.jsx
@@ -13,30 +13,90 @@ class Projects extends Component {
                     <div className="col-1-of-3">
                         <div className="card">
                             <div className="card-side card-side-front">
-                                Hello
+                                <div className="card-picture card-picture-1"></div>
+                                <h4 className="card-heading">
+                                    <span className="card-heading-span card-heading-span-1">
+                                        mLoops Spotify Explorer
+                                    </span>
+                                </h4>
+                                <div className="card-details">
+                                    <ul>
+                                        <li>afasdfasfds</li>
+                                        <li>afasdfasfds</li>
+                                        <li>afasdfasfds</li>
+                                        <li>afasdfasfds</li>
+                                        <li>afasdfasfds</li>
+                                    </ul>
+                                </div>
                             </div>
                             <div className="card-side card-side-back card-side-back-1">
-                                Hello
+                                <div className="card-cta">
+                                    <div className="card-details-box">
+                                        <p className="card-details-title">Search</p>
+                                        <p className="card-details-sub-title">The Best Music on Spotify</p>
+                                        <a href="#" className="btn btn--white">Details</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="col-1-of-3">
                         <div className="card">
                             <div className="card-side card-side-front">
-                                Hello
+                                <div className="card-picture card-picture-2"></div>
+                                <h4 className="card-heading">
+                                    <span className="card-heading-span card-heading-span-2">
+                                        Assessment Symptom Checker
+                                    </span>
+                                </h4>
+                                <div className="card-details">
+                                    <ul>
+                                        <li>afasdfasfds</li>
+                                        <li>afasdfasfds</li>
+                                        <li>afasdfasfds</li>
+                                        <li>afasdfasfds</li>
+                                        <li>afasdfasfds</li>
+                                    </ul>
+                                </div>
                             </div>
-                            <div className="card-side card-side-back">
-                                Hello
+                            <div className="card-side card-side-back card-side-back-2">
+                                <div className="card-cta">
+                                    <div className="card-details-box">
+                                        <p className="card-details-title">Diagnosis</p>
+                                        <p className="card-details-sub-title">Based on Your Symptoms</p>
+                                        <a href="#" className="btn btn--white">Details</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="col-1-of-3">
                         <div className="card">
                             <div className="card-side card-side-front">
-                                Hello
+                                <div className="card-picture card-picture-3"></div>
+                                <h4 className="card-heading">
+                                    <span className="card-heading-span card-heading-span-3">
+                                        Music In Real Time
+                                    </span>
+                                </h4>
+                                <div className="card-details">
+                                    <ul>
+                                        <li>afasdfasfds</li>
+                                        <li>afasdfasfds</li>
+                                        <li>afasdfasfds</li>
+                                        <li>afasdfasfds</li>
+                                        <li>afasdfasfds</li>
+                                    </ul>
+                                </div>
                             </div>
-                            <div className="card-side card-side-back">
-                                Hello
+                            <div className="card-side card-side-back card-side-back-3">
+                                <div className="card-cta">
+                                    <div className="card-details-box">
+                                        <p className="card-details-title">Play</p>
+                                        <p className="card-details-sub-title">Music In Real Time With Others</p>
+                                        <a href="#" className="btn btn--white">Details</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
