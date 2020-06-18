3289a9426f53ed2faf7bfe5b6b9e2e173f3a00d7
Fabricio <jfguardiacpw@gmail.com>
2020-06-18 13:05:20 -0700

    completed styling popup for projects

--- src/components/homepage/_popup.jsx
+++ src/components/homepage/_popup.jsx
@@ -5,7 +5,18 @@ class PopUp extends Component {
         return (
             <popup className="popup">
                 <div className="popup-content">
-                    
+                    <div className="popup-left">
+                        <img src="https://i.imgur.com/QII5QSq.png" alt="" className="popup-img"/>
+                        <img src="https://i.imgur.com/u1wEOBr.png" alt="" className="popup-img"/>
+                    </div>
+                    <div className="popup-right">
+                        <h2 className="heading-secondary u-margin-bottom-medium">About</h2>
+                        <h3 className="heading-tertiary u-margin-bottom-small">A MongoDB, Express, Node JS Stack App</h3>
+                        <p className="popup-text">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor asperiores quibusdam ex, doloribus, neque porro impedit pariatur minima omnis ipsa eos expedita necessitatibus magnam, libero consectetur dolores animi et accusamus!Temporibus sed harum veritatis voluptates illum nobis, explicabo deserunt magnam beatae. Amet, odit iusto! Aperiam maiores ipsa asperiores repudiandae? Quia quibusdam odit dolores cum aut porro harum perferendis! Accusantium, fugiat?
+                        </p>
+                        <a href="" className="btn btn--primary">Visit</a>
+                    </div>
                 </div>
             </popup>
         )
