bb39d820e503048e95331a0a3f83c861f0dfb63d
Fabricio <jfguardiacpw@gmail.com>
2020-06-19 15:12:19 -0700

    minor change

--- src/components/dashboard/_sectionAPI.jsx
+++ src/components/dashboard/_sectionAPI.jsx
@@ -15,7 +15,7 @@ class SectionAPI extends Component {
     spotifyLogin = () => {
         const localLogin = 'http://localhost:3001/api/music/login'
         const herokuLogin = 'https://codecallogic.herokuapp.com/api/music/login'
-        const domainLogin = 'https://www.codecallogic.com/'
+        const domainLogin = 'https://www.codecallogic.com/api/music/login'
         window.location=domainLogin
     }
 
