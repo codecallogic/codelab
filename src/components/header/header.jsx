001d7d83e0a77cd0855614259c252f2cdfe8bd2b
Fabricio <jfguardiacpw@gmail.com>
2020-07-14 15:31:13 -0700

    uploaded new photo

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -17,7 +17,7 @@ class Header extends Component {
                 <div className="header__text-box">
                     <h1 className="heading-primary">
                         <span className="heading-primary--main">Hi, I'm Fabricio</span>
-                        <span className="heading-primary--sub">I'm a full stack developer</span>
+                        <span className="heading-primary--sub">I'm a full stack developer, and I made this website</span>
                     </h1>
                     <Link to="section-about" activeClass="active" spy={true} smooth={true} offset={100} duraton={500}  href="/#" className="btn btn--white btn--animated" onClick={this.preventDefault}>Lets get started</Link>
                 </div>
